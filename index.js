2d9c06ef38eea04fe17acc1beff33c65208b18c6
Kimberley Blankenhorn <kimberley.blankenhorn@gmail.com>
2022-01-11 21:27:08 +0100

    Add process.argv for input

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ import chalk from 'chalk';
 // import { randomHexColor } from 'random-hex-color-generator';
 import randomColor from 'randomcolor'; // import the script
 
-const color = randomColor({
-  luminosity: 'random',
-  hue: 'random',
-});
-
 const inputColor = process.argv[2];
+
+const color = randomColor(
+  {
+    luminosity: 'random',
+    hue: 'random',
+  } + inputColor,
+);
 
 // const color = chalk.hex('#FF8800').bold('#FF8800');
 const multiLine = `  ############################
